b04ca0a05166d0967e4a71f6efc481cc01ce5299
Austin WIley <wileybeer@gmail.com>
2019-04-07 14:33:03 -0600

    pseudo code python app

--- src/s3_script.py
+++ src/s3_script.py
@@ -1,12 +1,12 @@
 
 #Create AWS account. Creat 2 buckets in the same region.  Add files to one Bucket and leave other emply.  Creat IAM role credintails and save them in invirionment varaibles.
 
-#put dreintials in ~.aws/credintials
+#Put credintials in ~.aws/credintials
 
-# import botot3
+#import botot3
 
-# prompt for input of three arguments: s3-bucket-1, s3-bucket-2 and file Size threshold in MB/
+#Prompt for input of three arguments: s3-bucket-1, s3-bucket-2 and file size threshold in MB
 
-#check each file size in bucket one and and if it is above threshhold copy to file second bucket
+#Check each file size in bucket one and and if it is above threshhold copy to file second bucket
 
-#use boto SDK to perform transfer
+#Use boto SDK to perform transfer
