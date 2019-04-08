9ae808ee8267e132fadc9b92e70b679e247c2ac9
Austin WIley <wileybeer@gmail.com>
2019-04-07 20:25:27 -0600

    cleanup python script

--- src/s3_script.py
+++ src/s3_script.py
@@ -7,30 +7,28 @@ s3 = boto3.resource('s3')
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
+print('Copy all files greater than the threshold size from one bucket to another.\n')
 # Bucket name that contains the files we will copy from
-src_bucket_name = input('Enter first: ')
+src_bucket_name = input('Enter source bucket name: ')
 # Bucket name that we will copy files into
-dest_bucket_name = input('Enter second: ')
+dest_bucket_name = input('Enter destination bucket name: ')
 # Threshld size im MB
-file_size = float(input('Enter mb size: '))*1000000
+file_size = float(input('Enter threshold size in MB: '))*1000000
 
 # Declare variable for number of files copied
 copied_files = 0
 
 # Check each file size in bucket one and if it is above file_size threshhold then copy to second bucket
 src_bucket = s3.Bucket(src_bucket_name)
-# src_bucket = s3.Bucket('wiley-test-one')
-for stuff in src_bucket.objects.all():
-    # print(stuff.key)
-    # print(stuff.size)
-    if stuff.size > file_size:
-        print(stuff.key)
-        # increment for every file that is abouve threshold
+# Loop through items in source bucket
+for item in src_bucket.objects.all():
+    if item.size > file_size:
+        # increment for every file that is above threshold
         copied_files += 1
         s3.meta.client.copy_object(
             Bucket=dest_bucket_name,
-            CopySource={'Bucket': src_bucket_name, 'Key': stuff.key},
-            Key=stuff.key
+            CopySource={'Bucket': src_bucket_name, 'Key': item.key},
+            Key=item.key
         )
 
 # print number of files copied
